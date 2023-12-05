29f29232bbd03c74cc2f2ec35f50dd83edaa4836
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2023-12-05 13:23:31 +0000

    Update mcu-swflash-run.sh

--- Neptune 4 MCU Firmware/new-method/mcu-swflash-run.sh
+++ Neptune 4 MCU Firmware/new-method/mcu-swflash-run.sh
@@ -38,6 +38,34 @@ else
     echo "Skipping backup."
 fi
 
+# Check if klipper.bin exists in the current directory
+KLIPPER_BIN=./klipper.bin
+if [ -f "$KLIPPER_BIN" ]; then
+    echo "klipper.bin file exists in the current directory. Do you want to flash it? (y/n)"
+    read -r flash_existing_bin
+    if [[ $flash_existing_bin == "y" ]]; then
+        # Flash the existing firmware
+	# Enable bootloader mode
+	echo "Enabling bootloader mode..."
+	sudo gpioset gpiochip1 15=0
+	sleep 0.5
+	sudo gpioset gpiochip1 14=1
+	sleep 0.5
+	sudo gpioset gpiochip1 15=1
+	sleep 0.5
+	sudo gpioset gpiochip1 14=0        
+
+	echo "Flashing existing firmware..."
+        stm32flash -w "$KLIPPER_BIN" -v  /dev/ttyS0
+        echo "Flashing complete"
+	gpioset gpiochip1 15=0; sleep 0.5; gpioset gpiochip1 15=1; sleep 1  
+        # Starting the Klipper service
+        echo "Starting the Klipper service..."
+        sudo service klipper start
+        exit 0
+    fi
+fi
+
 # Navigate to the Klipper directory and run make menuconfig
 echo "Running make menuconfig in the Klipper directory..."
 cd ~/klipper/
@@ -62,7 +90,7 @@ echo "Flashing new firmware to STM32F4..."
 stm32flash -w /home/mks/klipper/out/klipper.bin -v -S 0x08008000 -g 0x08000000 /dev/ttyS0
 
 echo "Flashing complete"
-
+gpioset gpiochip1 15=0; sleep 0.5; gpioset gpiochip1 15=1; sleep 1
 # Starting the Klipper service
 echo "Starting the Klipper service..."
 sudo service klipper start
