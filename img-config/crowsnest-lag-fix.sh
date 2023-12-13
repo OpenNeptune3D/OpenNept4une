fbebec8d96f6bd3edbb54932b5c68d4e4505a80b
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2023-12-13 17:16:08 +0000

    Create crowsnest-lag-fix.sh

---
+++ img-config/crowsnest-lag-fix.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Create symbolic link for ustreamer
+ln -sfn /usr/bin/ustreamer /home/mks/crowsnest/bin/ustreamer/ustreamer
+
+# Create symbolic link for ustreamer-dump
+ln -sfn /usr/bin/ustreamer-dump /home/mks/crowsnest/bin/ustreamer/ustreamer-dump
