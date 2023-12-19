e66a906f1d9982a4da7873a81312014354b4be57
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2023-12-19 15:15:48 +0000

    Update response_actions.py

--- display/response_actions.py
+++ display/response_actions.py
@@ -1,17 +1,41 @@
 response_actions = {
-	('65', '01', '01', 'ff', 'ff', 'ff'): "page 2", # Page 1 > Print Files Page 1
-	('65', '02', '02', 'ff', 'ff', 'ff'): "page 3", # Next Page Print Files ->
-	('65', '02', '01', 'ff', 'ff', 'ff'): "page 2", # Return to Print Files Page 1 <-
 	('65', '**', '00', 'ff', 'ff', 'ff'): "go_back", # Return to Previous Page
+	# MAIN PAGE OPTIONS (page 1)
+	('65', '01', '01', 'ff', 'ff', 'ff'): "page 2", # Page 1 > Print Files Page 1
 	('65', '01', '02', 'ff', 'ff', 'ff'): "page 8", # Page 1 > Prepare Page (Move)
+	('65', '01', '03', 'ff', 'ff', 'ff'): "page 11", # Page 1 > Settings Page
+	('65', '01', '04', 'ff', 'ff', 'ff'): "page 14", # Page 1 > Level Page #page 103 , 137?
+	# PRINT PAGE OPTIONS
+	('65', '02', '02', 'ff', 'ff', 'ff'): "page 3", # Next Page Print Files ->
+	('65', '02', '01', 'ff', 'ff', 'ff'): "page 2", # Previous Print Files Page <-
+	# PREPARE PAGE OPTIONS
 	('65', '08', '0f', 'ff', 'ff', 'ff'): "page 6", # Prepare Page (Move) > Prepare Page (Temp)
 	('65', '06', '07', 'ff', 'ff', 'ff'): "page 8", # Prepare Page (Temp) > Prepare Page (Move)
 	('65', '06', '08', 'ff', 'ff', 'ff'): "page 9", # Prepare Page (Temp) > Prepare Page (Extruder)
-	('65', '01', '03', 'ff', 'ff', 'ff'): "page 11", # Page 1 > Settings Page
-	('65', '0b', '01', 'ff', 'ff', 'ff'): "page 12", # Settings Page > Language
+	# SETTINGS PAGE OPTIONS
+	# LANGUAGE SELECT
+	('65', '0b', '01', 'ff', 'ff', 'ff'): "page 12", # Settings Page > Language ? page 31
+	# TEMPERATURE SETTINGS (Material)
 	('65', '0b', '02', 'ff', 'ff', 'ff'): "page 32", # Settings Page > Temperature Settings
+	('65', '20', '01', 'ff', 'ff', 'ff'): "page 33", # Temperature Settings > PLA
+	('65', '20', '02', 'ff', 'ff', 'ff'): "page 33", # Temperature Settings > ABS
+	('65', '20', '03', 'ff', 'ff', 'ff'): "page 33", # Temperature Settings > PETG
+	('65', '20', '04', 'ff', 'ff', 'ff'): "page 33", # Temperature Settings > TPU
+	('65', '20', '05', 'ff', 'ff', 'ff'): "page 33", # Temperature Settings > LEVEL
+	# ABOUT MACHINE
 	('65', '0b', '08', 'ff', 'ff', 'ff'): "page 35", # Settings Page > About Machine
+	# ADVANCED SETTINGS
 	('65', '0b', '09', 'ff', 'ff', 'ff'): "page 42", # Settings Page > Advanced Settings
+	# LIGHT CONTROL
 	('65', '0b', '03', 'ff', 'ff', 'ff'): "page 84", # Settings Page > Light Control
-	('65', '01', '04', 'ff', 'ff', 'ff'): "page 14", # Page 1 > Level Page #page 103 , 137?
+	('65', '54', '01', 'ff', 'ff', 'ff'): "Part_Light_toggle", # Light Control > Part Light toggle
+	('65', '54', '02', 'ff', 'ff', 'ff'): "Frame_Light_toggle", # Light Control > Frame Light toggle  
+	# FAN CONTROL
+	('5a', 'a5', '06', '83', '10', '3e'): "Fan_Toggle", # Settings Page > Fan Control Toggle
+	# MOTORS OFF
+	('65', '0b', '05', 'ff', 'ff', 'ff'): "Motor_OFF", # Settings Page > Motor-off
+	# FILAMENT DETECTOR 
+	('65', '0b', '06', 'ff', 'ff', 'ff'): "Filament_Detector_Toggle", # Settings Page > Filament Detector Toggle
+	# FACTORY SETTINGS
+	('65', '0b', '07', 'ff', 'ff', 'ff'): "factorysettingspage", # Settings Page >  Factory Settings
 }
