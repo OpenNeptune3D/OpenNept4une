f86c83fb9929450e4f4ea6a504a10ee9eecc4268
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2023-12-19 23:15:02 +0000

    Create display-service-installer.sh

---
+++ display/display-service-installer.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Define the service file path, script path, and log file path
+SERVICE_FILE="/etc/systemd/system/display.service"
+SCRIPT_PATH="/home/mks/OpenNept4une/display/display.py"
+LOG_FILE="/var/log/display.log"
+
+# Check if the script exists
+if [ ! -f "$SCRIPT_PATH" ]; then
+    echo "Error: Script $SCRIPT_PATH not found."
+    exit 1
+fi
+
+# Create the systemd service file
+echo "Creating systemd service file at $SERVICE_FILE..."
+cat <<EOF | sudo tee $SERVICE_FILE > /dev/null
+[Unit]
+Description=My Python Script Service
+After=network.target
+
+[Service]
+ExecStartPre=/bin/sleep 30
+ExecStart=/usr/bin/python3 $SCRIPT_PATH >> $LOG_FILE 2>&1
+WorkingDirectory=$(dirname $SCRIPT_PATH)
+Restart=always
+User=$(whoami)
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd to read new service file
+echo "Reloading systemd..."
+sudo systemctl daemon-reload
+
+# Enable and start the service
+echo "Enabling and starting the service..."
+sudo systemctl enable display.service
+sudo systemctl start display.service
+
+# Display the status of the service
+echo "Service status:"
+sudo systemctl status display.service
+
+echo "Service setup complete."
