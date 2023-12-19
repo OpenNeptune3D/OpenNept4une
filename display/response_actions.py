bd38021f5737a015d29f5d98dd8a44006bffc417
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2023-12-19 22:14:27 +0000

    Update response_actions.py

--- display/response_actions.py
+++ display/response_actions.py
@@ -1,42 +1,50 @@
 response_actions = {
-	('65', '**', '00', 'ff', 'ff', 'ff'): "go_back", # Return to Previous Page
-	# MAIN PAGE OPTIONS (page 1)
-	('65', '01', '01', 'ff', 'ff', 'ff'): "page 2", # Page 1 > Print Files Page 1
-	('65', '01', '02', 'ff', 'ff', 'ff'): "page 8", # Page 1 > Prepare Page (Move)
-	('65', '01', '03', 'ff', 'ff', 'ff'): "page 11", # Page 1 > Settings Page
-	('65', '01', '04', 'ff', 'ff', 'ff'): "page 14", # Page 1 > Level Page #page 103 , 137?
-	# PRINT PAGE OPTIONS
-	('65', '02', '02', 'ff', 'ff', 'ff'): "page 3", # Next Page Print Files ->
-	('65', '02', '01', 'ff', 'ff', 'ff'): "page 2", # Previous Print Files Page <-
-	# PREPARE PAGE OPTIONS
-	('65', '08', '0c', 'ff', 'ff', 'ff'): "printer.send_gcode('G28')", # Home Printer > Moonraker command
-	('65', '08', '0f', 'ff', 'ff', 'ff'): "page 6", # Prepare Page (Move) > Prepare Page (Temp)
-	('65', '06', '07', 'ff', 'ff', 'ff'): "page 8", # Prepare Page (Temp) > Prepare Page (Move)
-	('65', '06', '08', 'ff', 'ff', 'ff'): "page 9", # Prepare Page (Temp) > Prepare Page (Extruder)
-	# SETTINGS PAGE OPTIONS
-	# LANGUAGE SELECT
-	('65', '0b', '01', 'ff', 'ff', 'ff'): "page 12", # Settings Page > Language ? page 31
-	# TEMPERATURE SETTINGS (Material)
-	('65', '0b', '02', 'ff', 'ff', 'ff'): "page 32", # Settings Page > Temperature Settings
-	('65', '20', '01', 'ff', 'ff', 'ff'): "page 33", # Temperature Settings > PLA
-	('65', '20', '02', 'ff', 'ff', 'ff'): "page 33", # Temperature Settings > ABS
-	('65', '20', '03', 'ff', 'ff', 'ff'): "page 33", # Temperature Settings > PETG
-	('65', '20', '04', 'ff', 'ff', 'ff'): "page 33", # Temperature Settings > TPU
-	('65', '20', '05', 'ff', 'ff', 'ff'): "page 33", # Temperature Settings > LEVEL
-	# ABOUT MACHINE
-	('65', '0b', '08', 'ff', 'ff', 'ff'): "page 35", # Settings Page > About Machine
-	# ADVANCED SETTINGS
-	('65', '0b', '09', 'ff', 'ff', 'ff'): "page 42", # Settings Page > Advanced Settings
-	# LIGHT CONTROL
-	('65', '0b', '03', 'ff', 'ff', 'ff'): "page 84", # Settings Page > Light Control
-	('65', '54', '01', 'ff', 'ff', 'ff'): "printer.send_gcode('Part_Light_ON')", # Light Control > Part Light toggle
-	('65', '54', '02', 'ff', 'ff', 'ff'): "printer.send_gcode('Frame_Light_ON')", # Light Control > Frame Light toggle  
-	# FAN CONTROL
-	('5a', 'a5', '06', '83', '10', '3e'): "printer.send_gcode('M106 S255')", # Settings Page > Fan Control Toggle
-	# MOTORS OFF
-	('65', '0b', '05', 'ff', 'ff', 'ff'): "printer.send_gcode('M84')", # Settings Page > Motor-off
-	# FILAMENT DETECTOR 
-	('65', '0b', '06', 'ff', 'ff', 'ff'): "printer.send_gcode('SET_FILAMENT_SENSOR SENSOR=fila ENABLE=1')", # Settings Page > Filament Detector Toggle
-	# FACTORY SETTINGS
-	('65', '0b', '07', 'ff', 'ff', 'ff'): "factorysettingspage", # Settings Page >  Factory Settings
+    '65??00ffffff': "go_back",  # Return to Previous Page
+    # MAIN PAGE OPTIONS (page 1)
+    '650101ffffff': "page 2",  # Page 1 > Print Files Page 1
+    '650102ffffff': "page 8",  # Page 1 > Prepare Page (Move)
+    '650103ffffff': "page 11", # Page 1 > Settings Page
+    '650104ffffff': "page 14", # Page 1 > Level Page
+    # PRINT PAGE OPTIONS
+    '650202ffffff': "page 3",  # Next Page Print Files ->
+    '650201ffffff': "page 2",  # Previous Print Files Page <-
+    # PREPARE PAGE OPTIONS
+    '65080cffffff': "printer.send_gcode('G28')", # Home Printer > Moonraker command
+    '650808ffffff': "move_x_1mm", # X+ 1mm
+    '650809ffffff': "move_x_-1mm", # X- 1mm
+    '65080affffff': "move_y_1mm", # Y+ 1mm
+    '650807ffffff': "move_y_-1mm", # Y- 1mm
+    '650806ffffff': "move_z_1mm", # Z+ 1mm
+    '65080bffffff': "move_z_-1mm", # Z- 1mm
+    '650803ffffff': "page 10mm", # 10mm Move Page
+    '65080fffffff': "page 6",  # Prepare Page (Move) > Prepare Page (Temp)
+    '650607ffffff': "page 8",  # Prepare Page (Temp) > Prepare Page (Move)
+    '650608ffffff': "page 9",  # Prepare Page (Temp) > Prepare Page (Extruder)
+    # SETTINGS PAGE OPTIONS
+    # LANGUAGE SELECT
+    '650b01ffffff': "page 12", # Settings Page > Language
+    # TEMPERATURE SETTINGS (Material)
+    '650b02ffffff': "page 32", # Settings Page > Temperature Settings
+    '652001ffffff': "page 33", # Temperature Settings > PLA
+    '652002ffffff': "page 33", # Temperature Settings > ABS
+    '652003ffffff': "page 33", # Temperature Settings > PETG
+    '652004ffffff': "page 33", # Temperature Settings > TPU
+    '652005ffffff': "page 33", # Temperature Settings > LEVEL
+    # ABOUT MACHINE
+    '650b08ffffff': "page 35", # Settings Page > About Machine
+    # ADVANCED SETTINGS
+    '650b09ffffff': "page 42", # Settings Page > Advanced Settings
+    # LIGHT CONTROL
+    '650b03ffffff': "page 84", # Settings Page > Light Control
+    '655401ffffff': "toggle_part_light", # Light Control > Part Light toggle
+    '655402ffffff': "toggle_frame_light", # Light Control > Frame Light toggle
+    # FAN CONTROL
+    '5aa50683103e': "printer.send_gcode('M106 S255')", # Settings Page > Fan Control Toggle
+    'ffffff5aa506': "printer.send_gcode('M106 S0')", # Settings Page > Fan Control Toggle
+    # MOTORS OFF
+    '650b05ffffff': "printer.send_gcode('M84')", # Settings Page > Motor-off
+    # FILAMENT DETECTOR 
+    '650b06ffffff': "toggle_filament_sensor", # Settings Page > Filament Detector Toggle
+    # FACTORY SETTINGS
+    '650b07ffffff': "factorysettingspage", # Settings Page > Factory Settings
 }
