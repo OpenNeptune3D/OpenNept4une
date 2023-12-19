4bd89de49775b15acafed6e68986717ab00a5aa2
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2023-12-19 14:39:56 +0000

    Update display.py

--- display/display.py
+++ display/display.py
@@ -1,60 +1,102 @@
 #!/usr/bin/env python
 
-import serial
-import time
-import sys
+import serial, time
+from response_actions import response_actions
 
-def open_serial_port(port, baudrate):
+class NavigationController:
+    def __init__(self):
+        self.history = ["page 1"]  # Start with the main page
+
+    def go_to_page(self, page):
+        if page == "go_back":
+            self.go_back()
+        else:
+            self.history.append(page)
+            self.change_display(page)
+
+    def go_back(self):
+        if len(self.history) > 1:
+            self.history.pop()  # Remove current page
+            back_page = self.history[-1]
+            self.change_display(back_page)  # Go to the previous page
+        else:
+            print("Already at the main page.")
+
+    def change_display(self, page):
+        padding = [0xFF, 0xFF, 0xFF]
+        ser.write(str.encode(page))
+        ser.write(serial.to_bytes(padding))
+        print(f"Navigating to {page}")
+
+def handle_response(readData):
+    action = response_actions.get(tuple(readData))
+    if action:
+        nav_controller.go_to_page(action)
+    else:
+        # Handle wildcard pattern
+        if readData[0] == '65' and readData[2] == '00' and readData[3:] == ['ff', 'ff', 'ff']:
+            nav_controller.go_to_page("go_back")
+        else:
+            print("No action for response:", readData)
+
+SERIALPORT = "/dev/ttyS1"
+BAUDRATE = 115200
+
+ser = serial.Serial(SERIALPORT, BAUDRATE)
+
+ser.bytesize = serial.EIGHTBITS # number of bits per bytes
+ser.parity = serial.PARITY_NONE # set parity check: no parity
+ser.stopbits = serial.STOPBITS_ONE # number of stop bits
+ser.timeout = 2              # timeout block read
+ser.xonxoff = False     # disable software flow control
+ser.rtscts = False     # disable hardware (RTS/CTS) flow control
+ser.dsrdtr = False       # disable hardware (DSR/DTR) flow control
+ser.writeTimeout = 0     # timeout for write
+
+nav_controller = NavigationController()
+
+print('Starting Up...')
+
+try:
+    ser.close()
+    ser.open()
+
+except Exception as e:
+    print("Error open serial port: " + str(e))
+    exit()
+
+if ser.isOpen():
     try:
-        ser = serial.Serial(port, baudrate, timeout=2, writeTimeout=0)
-        ser.bytesize = serial.EIGHTBITS
-        ser.parity = serial.PARITY_NONE
-        ser.stopbits = serial.STOPBITS_ONE
-        ser.xonxoff = False
-        ser.rtscts = False
-        ser.dsrdtr = False
-        return ser
-    except serial.SerialException as e:
-        print(f"Error opening serial port {port}: {e}")
-        sys.exit(1)
+        ser.flushInput() # flush input buffer, discarding all its contents
+        ser.flushOutput() # flush output buffer, aborting current output
 
-def main():
-    SERIALPORT = "/dev/ttyS1"
-    BAUDRATE = 115200
+        # Send "page 1" command as default at the start
+        nav_controller.go_to_page("page 1")
 
-    with open_serial_port(SERIALPORT, BAUDRATE) as ser:
-        print('Starting Up...')
-        ser.flushInput()
-        ser.flushOutput()
+        while True:
+            print("Waiting for user interaction from Display (Ctrl-C for skip)")
+            readData = []
+            try:
+                while True:
+                    response = ser.read()
+                    if response:
+                        readData.append(response.hex())
+                    if len(readData) == 6:
+                        print("Data read from Display: ")
+                        print(readData)
+                        handle_response(readData)
+                        break
+                    else:
+                        time.sleep(0.2)
+            except KeyboardInterrupt:
+                print("\nInsert command (page 1, page 2, ...) for Elegoo Display: (Ctrl-C for exit)")
+                data = input()
+                nav_controller.go_to_page(data)
 
-        try:
-            while True:
-                command = input("Insert command for Elegoo Display (Ctrl-C to exit): ")
-                padding = [0xFF, 0xFF, 0xFF]
-                ser.write(command.encode() + serial.to_bytes(padding))
-                print(f"Write data: {command}")
+    except Exception as e:
+        print("Error communicating...: " + str(e))
+    finally:
+        ser.close()
 
-                time.sleep(0.1)
-                print("Waiting for response from Display (Ctrl-C to skip)")
-                readData = []
-
-                try:
-                    while True:
-                        response = ser.read()
-                        if response:
-                            readData.append(response.hex())
-                        if len(readData) == 6:
-                            print("Data read from Display: ", readData)
-                            break
-                        else:
-                            time.sleep(0.2)
-                except KeyboardInterrupt:
-                    print("\n")
-
-        except Exception as e:
-            print(f"Error communicating: {e}")
-        finally:
-            print("Closing serial port.")
-
-if __name__ == "__main__":
-    main()
+else:
+    print("Cannot open serial port!")
