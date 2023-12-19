dd17d4b333dea40540738e9639fa9f7b4a69af5d
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2023-12-19 22:14:19 +0000

    Update display.py

--- display/display.py
+++ display/display.py
@@ -1,107 +1,124 @@
 import serial
 import time
 import moonrakerpy as moonpy
+import re
 
 from response_actions import response_actions
 
 class NavigationController:
-    def __init__(self, printer):
-        self.history = ["page 1"]  # Start with the main page
+    def __init__(self, printer, serial_device):
+        self.history = ["page 1"]
         self.printer = printer
+        self.serial_device = serial_device
+        self.part_light_state = False
+        self.frame_light_state = False
+        self.fan_state = False
+        self.filament_sensor_state = False
 
-    def go_to_page(self, action):
-        if action.startswith("printer.send_gcode"):
-            gcode = action.split("'")[1]  # Extract G-code from the action string
-            self.printer.send_gcode(gcode)
+    def move_axis(self, axis, distance):
+        self.printer.send_gcode('G91')  # Set to relative positioning
+        self.printer.send_gcode(f'G1 {axis}{distance}')  # Move axis
+        self.printer.send_gcode('G90')  # Set back to absolute positioning
+
+    def execute_action(self, action):
+        if action.startswith("move_"):
+            axis = action.split('_')[1].upper()  # 'X', 'Y', or 'Z'
+            distance = action.split('_')[2]  # e.g., '1mm', '-10mm'
+            self.move_axis(axis, distance)
+        elif action == "toggle_part_light":
+            self._toggle_light("Part_Light", self.part_light_state)
+            self.part_light_state = not self.part_light_state
+        elif action == "toggle_frame_light":
+            self._toggle_light("Frame_Light", self.frame_light_state)
+            self.frame_light_state = not self.frame_light_state
+        elif action == "toggle_filament_sensor":
+            self._toggle_filament_sensor()
+        elif action.startswith("printer.send_gcode"):
+            self._send_gcode(action)
         elif action == "go_back":
-            self.go_back()
+            self._go_back()
         else:
-            self.history.append(action)
-            self.change_display(action)
+            self._navigate_to_page(action)
 
-    def go_back(self):
+    def _toggle_light(self, light_name, current_state):
+        gcode = f"{light_name}_{'OFF' if current_state else 'ON'}"
+        self.printer.send_gcode(gcode)
+
+    def _toggle_filament_sensor(self):
+        gcode = f"SET_FILAMENT_SENSOR SENSOR=fila ENABLE={'0' if self.filament_sensor_state else '1'}"
+        self.printer.send_gcode(gcode)
+        self.filament_sensor_state = not self.filament_sensor_state
+
+    def _send_gcode(self, action):
+        gcode = action.split("'")[1]
+        self.printer.send_gcode(gcode)
+
+    def _go_back(self):
         if len(self.history) > 1:
-            self.history.pop()  # Remove current page
+            self.history.pop()
             back_page = self.history[-1]
-            self.change_display(back_page)  # Go to the previous page
+            self._change_display(back_page)
         else:
             print("Already at the main page.")
 
-    def change_display(self, page):
+    def _navigate_to_page(self, page):
+        if self.history[-1] != page:
+            self.history.append(page)
+        self._change_display(page)
+
+    def _change_display(self, page):
         padding = [0xFF, 0xFF, 0xFF]
-        ser.write(str.encode(page))
-        ser.write(serial.to_bytes(padding))
+        self.serial_device.write(str.encode(page))
+        self.serial_device.write(serial.to_bytes(padding))
         print(f"Navigating to {page}")
 
-def handle_response(readData):
-    action = response_actions.get(tuple(readData))
-    if action:
-        if "printer.send_gcode" in action:
-            nav_controller.go_to_page(action)
-        else:
-            nav_controller.go_to_page(action)
+
+def generate_key(readData):
+    return ''.join(readData)
+
+def match_key(pattern, key):
+    pattern_regex = pattern.replace('??', '..')
+    return re.match(pattern_regex, key) is not None
+
+def handle_response(readData, nav_controller):
+    action_key = generate_key(readData)
+    for key in response_actions.keys():
+        if match_key(key, action_key):
+            nav_controller.execute_action(response_actions[key])
+            break
     else:
-        # Handle wildcard pattern
-        if readData[0] == '65' and readData[2] == '00' and readData[3:] == ['ff', 'ff', 'ff']:
-            nav_controller.go_to_page("go_back")
-        else:
-            print("No action for response:", readData)
+        print("No action for response:", readData)
 
 printer = moonpy.MoonrakerPrinter('http://127.0.0.1')
+ser = serial.Serial("/dev/ttyS1", 115200, timeout=2, writeTimeout=0)
 
-SERIALPORT = "/dev/ttyS1"
-BAUDRATE = 115200
-
-ser = serial.Serial(SERIALPORT, BAUDRATE)
-ser.bytesize = serial.EIGHTBITS  # number of bits per bytes
-ser.parity = serial.PARITY_NONE  # set parity check: no parity
-ser.stopbits = serial.STOPBITS_ONE  # number of stop bits
-ser.timeout = 2  # timeout block read
-ser.xonxoff = False  # disable software flow control
-ser.rtscts = False  # disable hardware (RTS/CTS) flow control
-ser.dsrdtr = False  # disable hardware (DSR/DTR) flow control
-ser.writeTimeout = 0  # timeout for write
-
-nav_controller = NavigationController(printer)
+nav_controller = NavigationController(printer, ser)
 
 print('Starting Up...')
 
 try:
     ser.close()
     ser.open()
-except Exception as e:
-    print("Error open serial port: " + str(e))
-    exit()
-
-if ser.isOpen():
-    try:
-        ser.flushInput()  # flush input buffer, discarding all its contents
-        ser.flushOutput()  # flush output buffer, aborting current output
-        nav_controller.go_to_page("page 1")
-
-        while True:
-            print("Waiting for user interaction from Display (Ctrl-C for skip)")
-            readData = []
-            try:
-                while True:
-                    response = ser.read()
-                    if response:
-                        readData.append(response.hex())
-                    if len(readData) == 6:
-                        print("Data read from Display: ")
-                        print(readData)
-                        handle_response(readData)
+    nav_controller.execute_action("page 1")
+    while True:
+        print("Waiting for user interaction from Display (Ctrl-C to navigate)")
+        readData = []
+        try:
+            while True:
+                response = ser.read()
+                if response:
+                    readData.append(response.hex())
+                    if len(readData) >= 6:
+                        print("Data read from Display: " + ''.join(readData))
+                        handle_response(readData, nav_controller)
                         break
-                    else:
-                        time.sleep(0.2)
-
-            except KeyboardInterrupt:
-                print("\nInsert command (page 1, page 2, ...) for Elegoo Display: (Ctrl-C for exit)")
-                data = input()
-                nav_controller.go_to_page(data)
-    except Exception as e:
-        print("Error communicating...: " + str(e))
-    finally:
-        ser.close()
-else:
-    print("Cannot open serial port!")
+                else:
+                    time.sleep(0.2)
+        except KeyboardInterrupt:
+            print("\nEnter page number to navigate to (e.g., 'page 1', 'page 2', etc.): ")
+            page = input().strip()
+            nav_controller.execute_action(page)
+except Exception as e:
+    print("Error communicating...: " + str(e))
+finally:
+    ser.close()
