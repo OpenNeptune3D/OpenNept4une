814b3bed10ca4b5f10dc5abb9c2d92a1d7a31c4d
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2023-12-26 13:52:11 +0000

    Update usb-storage-automount.sh

--- img-config/usb-storage-automount.sh
+++ img-config/usb-storage-automount.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 
-# Define the base directory for mount points
-base_mount_dir="/home/mks/printer_data/gcodes/usb"
-
-# Create a new udev rule file
-udev_rule_file="/etc/udev/rules.d/99-usb_automount.rules"
-
-# Function to create a udev rule
-create_udev_rule() {
-    echo 'ACTION=="add", SUBSYSTEM=="block", ENV{ID_FS_USAGE}=="filesystem", ENV{ID_FS_LABEL}!="", RUN+="/usr/bin/systemd-mount --no-block --automount=yes --collect --mkdir --options=ro $env{DEVNAME} '$base_mount_dir/%E{ID_FS_LABEL}'"' > $udev_rule_file
-    echo 'ACTION=="add", SUBSYSTEM=="block", ENV{ID_FS_USAGE}=="filesystem", ENV{ID_FS_LABEL}=="", RUN+="/usr/bin/systemd-mount --no-block --automount=yes --collect --mkdir --options=ro $env{DEVNAME} '$base_mount_dir/%E{ID_SERIAL}'"' >> $udev_rule_file
-}
-
-# Check if the script is run as root
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
 # Create the base mount directory if it doesn't exist
-mkdir -p $base_mount_dir
+mkdir -p ~/home/mks/printer_data/gcodes/USB
 
-# Create the udev rule
-create_udev_rule
+sudo -u mks echo 'ACTION=="add", SUBSYSTEMS=="usb", SUBSYSTEM=="block", KERNEL=="sd*1", ENV{ID_FS_USAGE}=="filesystem", RUN{program}+="/usr/bin/systemd-mount --no-block --automount=yes --collect --options ro,nofail $devnode /home/mks/printer_data/gcodes/USB"' | sudo tee -a /etc/udev/rules.d/99-usb_automount.rules
 
 # Reload udev rules
-udevadm control --reload-rules && udevadm trigger
+sudo udevadm control --reload-rules && sudo udevadm trigger
 
 echo "Udev rule for USB automount with read-only option is configured."
