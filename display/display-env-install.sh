aa94d376e3b83ab9217d30a4a7966949b77f0797
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2023-12-28 21:17:11 +0000

    Create display-env-install.sh

---
+++ display/display-env-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install python3.11-venv -y
+
+# Navigate to the script directory
+cd /home/mks/OpenNept4une/display
+
+# Create a Python virtual environment in the current directory
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install required Python packages
+pip install pyserial moonrakerpy
+
+# Deactivate the virtual environment
+deactivate
