27f08c2241ee3e1d5d264644a48d37cfc17e5e4c
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-01-17 17:49:46 +0000

    mounts usb .gcode s in gcodes dir

--- img-config/usb-storage-automount.sh
+++ img-config/usb-storage-automount.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-# Create the base mount directory if it doesn't exist
-mkdir -p ~/home/mks/printer_data/gcodes/USB
+# Base directories
+MOUNT_DIR="/home/mks/printer_data/gcodes/USB"
+SYMLINK_DIR="/home/mks/printer_data/gcodes"
+RULE_FILE="/etc/udev/rules.d/99-usb_automount.rules"
 
-sudo -u mks echo 'ACTION=="add", SUBSYSTEMS=="usb", SUBSYSTEM=="block", KERNEL=="sd*1", ENV{ID_FS_USAGE}=="filesystem", RUN{program}+="/usr/bin/systemd-mount --no-block --automount=yes --collect --options ro,nofail $devnode /home/mks/printer_data/gcodes/USB"' | sudo tee -a /etc/udev/rules.d/99-usb_automount.rules
+create_symlink() {
+    if [ -d "$MOUNT_DIR" ]; then
+        # Find .gcode files and create symlinks
+        find "$MOUNT_DIR" -type f -name "*.gcode" -exec ln -sfn {} "$SYMLINK_DIR" \;
+    fi
+}
 
-# Reload udev rules
+# Function to remove symlinks
+remove_symlink() {
+    find "$SYMLINK_DIR" -type l -exec test ! -e {} \; -delete
+}
+
+# Check if script is being run with a flag
+if [ "$1" = "insert" ]; then
+    create_symlink
+    exit 0
+elif [ "$1" = "remove" ]; then
+    remove_symlink
+    exit 0
+fi
+
+# If no valid argument is provided, run the following
+# Create or overwrite udev rule for USB automount and symlink handling
+echo 'ACTION=="add", SUBSYSTEMS=="usb", SUBSYSTEM=="block", KERNEL=="sd*1", ENV{ID_FS_USAGE}=="filesystem", RUN+="/usr/bin/systemd-mount --no-block --automount=yes --collect --options ro,nofail $devnode /home/mks/printer_data/gcodes/USB", RUN+="/home/mks/OpenNept4une/img-config/usb-storage-automount.sh insert"' | sudo tee $RULE_FILE
+echo 'ACTION=="remove", SUBSYSTEMS=="usb", SUBSYSTEM=="block", KERNEL=="sd*1", ENV{ID_FS_USAGE}=="filesystem", RUN+="/home/mks/OpenNept4une/img-config/usb-storage-automount.sh remove"' | sudo tee -a $RULE_FILE
+
 sudo udevadm control --reload-rules && sudo udevadm trigger
-
-echo "Udev rule for USB automount with read-only option is configured."
+echo "Udev rules for USB automount and symlink handling are configured."
