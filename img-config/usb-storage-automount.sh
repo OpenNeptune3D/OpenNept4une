4fcc7315d8944a3820496cb680a23dd06e55ec17
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-01-25 23:19:31 +0000

    Update usb-storage-automount.sh

--- img-config/usb-storage-automount.sh
+++ img-config/usb-storage-automount.sh
@@ -4,7 +4,7 @@
 # Create the base mount directory if it doesn't exist
 mkdir -p ~/printer_data/gcodes/USB
 sudo rm /etc/udev/rules.d/99-usb_automount.rules
-sudo -u mks echo 'ACTION=="add", SUBSYSTEMS=="usb", SUBSYSTEM=="block", KERNEL=="sd*1", ENV{ID_FS_USAGE}=="filesystem", RUN{program}+="/usr/bin/systemd-mount --no-block --automount=yes --collect --options ro,nofail $devnode ~/printer_data/gcodes/USB"' | sudo tee -a /etc/udev/rules.d/99-usb_automount.rules
+sudo -u mks echo 'ACTION=="add", SUBSYSTEMS=="usb", SUBSYSTEM=="block", KERNEL=="sd*1", ENV{ID_FS_USAGE}=="filesystem", RUN{program}+="/usr/bin/systemd-mount --no-block --automount=yes --collect --options ro,nofail $devnode /home/mks/printer_data/gcodes/USB"' | sudo tee -a /etc/udev/rules.d/99-usb_automount.rules
 
 # Reload udev rules
 sudo udevadm control --reload-rules && sudo udevadm trigger
