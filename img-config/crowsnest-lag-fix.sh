5e273421848e4ba6389782c33245f29d914b23ac
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-01-29 12:16:45 +0000

    Update crowsnest-lag-fix.sh

--- img-config/crowsnest-lag-fix.sh
+++ img-config/crowsnest-lag-fix.sh
@@ -11,8 +11,8 @@ LOG_FILE="${HOME}/printer_data/logs/crowsnest.log"
 CONF_FILE="${HOME}/printer_data/config/crowsnest.conf"
 
 # Search for the video device in the log file
-# The grep command looks for lines that map the camera to a video device, capturing the last part that starts with '/dev/video'
-DEVICE_PATH=$(grep -o ' -> /dev/video[0-9]\+' "$LOG_FILE" | tail -1) # Using 'tail -1' to get the last match, if multiple
+# The grep -P command uses Perl-compatible regex to look for lines that map the camera to a video device, capturing only the '/dev/video[0-9]+' part
+DEVICE_PATH=$(grep -oP '(?<= -> )/dev/video[0-9]+' "$LOG_FILE" | tail -1) # Using 'tail -1' to get the last match, if multiple
 
 # Check if a device path was found
 if [ -z "$DEVICE_PATH" ]; then
@@ -24,6 +24,6 @@ echo "$DEVICE_PATH"
 sleep 3
 # Update the configuration file with the detected video device path
 # This sed command looks for the line starting with 'device:' and replaces it with the new device path
-sed -i "s|device: /dev/video[0-9]\+|device: $DEVICE_PATH|" "$CONF_FILE"
+sed -i "s|device: \+/dev/video[0-9]\+|device: $DEVICE_PATH|" "$CONF_FILE"
 echo
 echo "Updated configuration file with video device path: $DEVICE_PATH"
