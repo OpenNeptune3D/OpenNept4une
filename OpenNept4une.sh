a6ba2f090d2868e76ce776f1269b9b86c9df341c
Phillip Thelen <phillip@thelen.space>
2024-01-30 18:28:56 +0100

    Allow OpenNept4une.sh to be called with arguments to enable automation

--- OpenNept4une.sh
+++ OpenNept4une.sh
@@ -11,20 +11,28 @@ BASE_IMAGE_INSTALLER="${HOME}/OpenNept4une/img-config/base_image_configuration.s
 DE_ELEGOO_IMAGE_CLEANSER="${HOME}/OpenNept4une/img-config/de_elegoo_cleanser.sh"
 FLAG_FILE="/boot/.OpenNept4une.txt"
 
-# Image Fixes 
-sudo usermod -aG gpio,spiusers mks &>/dev/null
-sudo rm -f /usr/local/bin/set_gpio.sh
+# Command line arguments
+PRINTER_MODEL=
+MOTOR_CURRENT=
+PCB_VERSION=
+SAY_YES=false
 
-# Ensure the flag file exists and update its timestamp
-sudo touch "$FLAG_FILE"
+run_fixes() {
+    # Image Fixes 
+    sudo usermod -aG gpio,spiusers mks &>/dev/null
+    sudo rm -f /usr/local/bin/set_gpio.sh
 
-# Get system information
-SYSTEM_INFO=$(uname -a)
+    # Ensure the flag file exists and update its timestamp
+    sudo touch "$FLAG_FILE"
 
-# Check if the system information is already in the flag file and append it if not
-if ! sudo grep -qF "$SYSTEM_INFO" "$FLAG_FILE"; then
-    echo "$SYSTEM_INFO" | sudo tee -a "$FLAG_FILE" > /dev/null
-fi
+    # Get system information
+    SYSTEM_INFO=$(uname -a)
+
+    # Check if the system information is already in the flag file and append it if not
+    if ! sudo grep -qF "$SYSTEM_INFO" "$FLAG_FILE"; then
+        echo "$SYSTEM_INFO" | sudo tee -a "$FLAG_FILE" > /dev/null
+    fi
+}
 
 # ASCII art for OpenNept4une 
 OPENNEPT4UNE_ART=$(cat <<'EOF'
@@ -68,7 +76,7 @@ update_repo() {
     if [ "$LOCAL" != "$REMOTE" ]; then
         echo "Updates are available for the repository."
         read -p "Would you like to update the repository? (y/n): " -r
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
+        if [[ $REPLY =~ ^[Yy]$ || $SAY_YES = true ]]; then
             echo "Updating repository..."
             git reset --hard
             git clean -fd
@@ -84,8 +92,6 @@ update_repo() {
     fi
     echo "======================================"
 }
-
-update_repo
 
 advanced_more() {
 clear_screen
@@ -133,10 +139,12 @@ android_rules() {
     clear_screen
     echo -e "\033[0;33m$OPENNEPT4UNE_ART\033[0m"
     echo "======================================"
-    echo "Do you want to install the android ADB rules? (may fix klipperscreen issues)"
-    read -p "Enter 'y' to install, any other key to skip: " install_android_rules
+    if [ $SAY_YES = false ]; then
+        echo "Do you want to install the android ADB rules? (may fix klipperscreen issues)"
+        read -p "Enter 'y' to install, any other key to skip: " install_android_rules
+    fi
 
-    if [[ $install_android_rules == "y" ]]; then
+    if [[ $install_android_rules =~ ^[Yy]$ || $SAY_YES = true ]]; then
         echo "Running ADB Rule Installer..."
         if [ -f "$ANDROID_RULE_INSTALLER" ]; then
             "$ANDROID_RULE_INSTALLER"
@@ -151,10 +159,12 @@ crowsnest_fix() {
     clear_screen
     echo -e "\033[0;33m$OPENNEPT4UNE_ART\033[0m"
     echo "======================================"
-    echo "Do you want to install the crowsnest fps fix?"
-    read -p "Enter 'y' to install, any other key to skip: " install_crowsnest_fix
+    if [ $SAY_YES = false ]; then
+        echo "Do you want to install the crowsnest fps fix?"
+        read -p "Enter 'y' to install, any other key to skip: " install_crowsnest_fix
+    fi
 
-    if [[ $install_crowsnest_fix == "y" ]]; then
+    if [[ $install_crowsnest_fix =~ ^[Yy]$ || $SAY_YES = true ]]; then
         echo "Running crowsnest Fix Installer..."
         if [ -f "$CROWSNEST_FIX_INSTALLER" ]; then
             "$CROWSNEST_FIX_INSTALLER"
@@ -169,10 +179,12 @@ base_image_config() {
     clear_screen
     echo -e "\033[0;33m$OPENNEPT4UNE_ART\033[0m"
     echo "======================================"
-    echo "Do you want to configure a base/fresh armbian image that you compiled?"
-    read -p "Enter 'y' to install, any other key to skip: " install_base_image_config
+    if [ $SAY_YES = false ]; then
+        echo "Do you want to configure a base/fresh armbian image that you compiled?"
+        read -p "Enter 'y' to install, any other key to skip: " install_base_image_config
+    fi
 
-    if [[ $install_base_image_config == "y" ]]; then
+    if [[ $install_base_image_config =~ ^[Yy]$ || $SAY_YES = true ]]; then
         echo "Running base/fresh image Installer..."
         if [ -f "$BASE_IMAGE_INSTALLER" ]; then
             "$BASE_IMAGE_INSTALLER"
@@ -189,9 +201,11 @@ de_elegoo_image_cleanser() {
     echo "======================================"
     echo "DO NOT run this on an OpenNept4une GitHub Image, for Elegoo images only!"
     echo "Continue at your own risk! High chance of requiring a eMMC re-flash!"
-    read -p "Enter 'y' to install, any other key to skip: " install_de_elegoo_image_cleanser
+    if [ $SAY_YES = false ]; then
+        read -p "Enter 'y' to install, any other key to skip: " install_de_elegoo_image_cleanser
+    fi
 
-    if [[ $install_de_elegoo_image_cleanser == "y" ]]; then
+    if [[ $install_de_elegoo_image_cleanser =~ ^[Yy]$ || $SAY_YES = true ]]; then
         echo "Running De-Elegoo Script..."
         if [ -f "$DE_ELEGOO_IMAGE_CLEANSER" ]; then
             "$DE_ELEGOO_IMAGE_CLEANSER"
@@ -208,9 +222,10 @@ armbian_resize() {
     echo "======================================"
     echo "The system will reboot then resize after running this script"
     echo "allow it some time to complete (15min) before powering off or rebooting again"
-    read -p "Enter 'y' to run, any other key to skip: " run_armbian_resize
-
-    if [[ $run_armbian_resize == "y" ]]; then
+    if [ $SAY_YES = false ]; then
+        read -p "Enter 'y' to run, any other key to skip: " run_armbian_resize
+    fi
+    if [[ $run_armbian_resize =~ ^[Yy]$ || $SAY_YES = true ]]; then
         echo "Running Resize Script..."
         if sudo systemctl enable armbian-resize-filesystem; then
             sudo reboot
@@ -226,10 +241,11 @@ install_screen_service() {
     clear_screen
     echo -e "\033[0;33m$OPENNEPT4UNE_ART\033[0m"
     echo "======================================"
-    echo "Do you want to install the Touch-Screen Display Service? (BETA)"
-    read -p "Enter 'y' to install, any other key to skip: " install_screen
-
-    if [[ $install_screen == "y" ]]; then
+    if [ $SAY_YES = false ]; then
+        echo "Do you want to install the Touch-Screen Display Service? (BETA)"
+        read -p "Enter 'y' to install, any other key to skip: " install_screen
+    fi
+    if [[ $install_screen =~ ^[Yy]$ || $SAY_YES = true ]]; then
         echo "Installing Touch-Screen Service..."
         if [ -f "$DISPLAY_SERVICE_INSTALLER" ]; then
             sudo rm -rf ~/OpenNept4une/display/venv
@@ -246,14 +262,19 @@ update_mcu_rpi_fw() {
     clear_screen
     echo -e "\033[0;33m$OPENNEPT4UNE_ART\033[0m"
     echo "======================================"
-    echo "Do you want to update the MCU or Virtual MCU?"
-    echo ""
-    read -p "Enter 'y' to install, any other key to skip: " install_mcu_rpi
+    if [ $SAY_YES = false ]; then
+        echo "Do you want to update the MCU or Virtual MCU?"
+        read -p "Enter 'y' to install, any other key to skip: " install_mcu_rpi
+    fi
 
-    if [[ $install_mcu_rpi == "y" ]]; then
+    if [[ $install_mcu_rpi =~ ^[Yy]$ || $SAY_YES = true ]]; then
         echo "Running MCU / MCU RPi Installer..."
         if [ -f "$MCU_RPI_INSTALLER" ]; then
-            "$MCU_RPI_INSTALLER"
+            if [[ $SAY_YES = true ]]; then
+                "$MCU_RPI_INSTALLER" "Virtual RPi"
+            else
+                "$MCU_RPI_INSTALLER"
+            fi
         else
             echo "Error: Virtual / MCU installer script not found."
         fi
@@ -265,10 +286,12 @@ usb_auto_mount() {
     clear_screen
     echo -e "\033[0;33m$OPENNEPT4UNE_ART\033[0m"
     echo "======================================"
-    echo "Do you want to auto mount USB drives? (Folder will mount as 'USB' in fluid 'Jobs' Tab)"
-    read -p "Enter 'y' to install, any other key to skip: " install_usb_auto_mount
+    if [ $SAY_YES = false ]; then
+        echo "Do you want to auto mount USB drives? (Folder will mount as 'USB' in fluid 'Jobs' Tab)"
+        read -p "Enter 'y' to install, any other key to skip: " install_usb_auto_mount
+    fi
 
-    if [[ $install_usb_auto_mount == "y" ]]; then
+    if [[ $install_usb_auto_mount =~ ^[Yy]$ || $SAY_YES = true ]]; then
         echo "Running USB Auto Mount Installer..."
         if [ -f "$USB_STORAGE_AUTOMOUNT" ]; then
             "$USB_STORAGE_AUTOMOUNT"
@@ -338,12 +361,20 @@ apply_configuration() {
         echo -e "\nError: Invalid DTB file selection.\n"
         return 1
     fi
-
-    echo -e "\nDo you wish to install the latest KAMP/moonraker/fluiddGUI configurations? (y/n)\n"
-    read -r REPLY
-
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        echo -e "\nInstalling latest configurations...\n"
+    
+    # User prompt for installing KAMP/moonraker and fluidd GUI configuration
+    if [ $SAY_YES = false ]; then
+        echo ""
+        echo "Do you wish to install the latest KAMP/moonraker/fluiddGUI configurations? (y/n)"
+        echo ""
+        read -p "If this is a first-time install, it is recommended. If just updating printer.cfg & you have custom KAMP configurations, it is best to skip: " user_choice
+        echo ""
+    fi
+    
+    if [[ "$user_choice" =~ ^[Yy]$ || $SAY_YES = true ]]; then
+        # Commands to install the latest configurations
+        echo "Installing latest configurations..."
+        echo ""
         cp -r ~/OpenNept4une/img-config/printer-data/* ~/printer_data/config/
         mv ~/printer_data/config/data.mdb ~/printer_data/database/data.mdb
     else
@@ -354,12 +385,28 @@ apply_configuration() {
 install_printer_cfg() {
     clear_screen
     echo -e "\033[0;33m$OPENNEPT4UNE_ART\033[0m"
-    echo "Please select your machine type:"
-    echo "1) Neptune4"
-    echo "2) Neptune4 Pro"
-    echo "3) Neptune4 Plus"
-    echo "4) Neptune4 Max"
-    read -p "Enter your choice (1-4): " MACHINE_TYPE
+    if [[ -z "$PRINTER_MODEL" && $SAY_YES = false ]]; then
+        echo "Please select your machine type:"
+        echo "1) Neptune4"
+        echo "2) Neptune4 Pro"
+        echo "3) Neptune4 Plus"
+        echo "4) Neptune4 Max"
+        read -p "Enter your choice (1-4): " MACHINE_TYPE
+    else
+        case $PRINTER_MODEL in
+            "N4"|"Neptune4"|"Neptune4")
+                MACHINE_TYPE=1 ;;
+            "N4Pro"|"Neptune4Pro"|"Neptune4 Pro")
+                MACHINE_TYPE=2 ;;
+            "N4Plus"|"Neptune4Plus"|"Neptune4 Plus")
+                MACHINE_TYPE=3 ;;
+            "N4Max"|"Neptune4Max"|"Neptune4 Max")
+                MACHINE_TYPE=4 ;;
+            *)
+                echo "Invalid machine type. Please try again."
+                exit 1;;
+        esac
+    fi
 
     PRINTER_CFG_DEST="${HOME}/printer_data/config"
     DTB_DEST="/boot/dtb/rockchip/rk3328-roc-cc.dtb"
@@ -381,17 +428,27 @@ install_printer_cfg() {
     }
 
     stepper_motor_current() {
-        read -p "Enter stepper motor current (0.8 or 1.2): " MOTOR_CURRENT
+        if [[ -z "$MOTOR_CURRENT" && $SAY_YES = false ]]; then
+            read -p "Enter stepper motor current (0.8 or 1.2): " MOTOR_CURRENT
+        fi
         if [[ "$MOTOR_CURRENT" != "0.8" && "$MOTOR_CURRENT" != "1.2" ]]; then
             echo "Invalid motor current selection. Please enter 0.8 or 1.2."
+            if [[ $SAY_YES = true ]]; then
+                exit 1
+            fi
             stepper_motor_current
         fi
     }
 
     pcb_version() {
-        read -p "Enter PCB Version (1.0 or 1.1): " PCB_VERSION
+        if [[ -z "$PCB_VERSION" && $SAY_YES = false ]]; then
+            read -p "Enter PCB Version (1.0 or 1.1): " PCB_VERSION
+        fi
         if [[ "$PCB_VERSION" != "1.0" && "$PCB_VERSION" != "1.1" ]]; then
             echo "Invalid PCB selection. Please enter 1.0 or 1.1."
+            if [[ $SAY_YES = true ]]; then
+                exit 1
+            fi
             pcb_version
         fi
     }
@@ -451,9 +508,11 @@ install_printer_cfg() {
 reboot_system() {
     clear_screen
     echo -e "\033[0;33m$OPENNEPT4UNE_ART\033[0m"
-    echo "The system needs to be rebooted to continue. Reboot now? (y/n)"
-    read -p "Enter your choice (highly advised): " REBOOT_CHOICE
-    if [[ "$REBOOT_CHOICE" == "y" ]]; then
+    if [ $SAY_YES = false ]; then
+        echo "The system needs to be rebooted to continue. Reboot now? (y/n)"
+        read -p "Enter your choice (highly advised): " REBOOT_CHOICE
+    fi
+    if [[ "$REBOOT_CHOICE" =~ ^[Yy]$ || $SAY_YES = true ]]; then
         echo "System will reboot now."
         sudo reboot
     else
@@ -464,6 +523,30 @@ reboot_system() {
 # Function to configure WiFi
 wifi_config() {
 sudo nmtui
+}
+
+print_help() {
+    echo "Usage: $0 [OPTIONS] COMMAND"
+    echo "OpenNept4une configuration script."
+    echo ""
+    echo "  -y, --yes                   Automatic yes to prompts."
+    echo "      --printer_model=MODEL   Specify the printer model."
+    echo "      --motor_current=VALUE   Specify the stepper motor current."
+    echo "      --pcb_version=VALUE     Specify the PCB version."
+    echo "  -h, --help                  Display this help and exit."
+    echo ""
+    echo "Commands:"
+    echo "  install_printer_cfg         Install/Update latest OpenNept4une Printer.cfg + other configs"
+    echo "  usb_auto_mount              Enable USB Storage AutoMount"
+    echo "  update_mcu_rpi_fw           Update MCU & (Virtual) MCU RPi Firmware"
+    echo "  install_screen_service      Install/Update Touch-Screen Display Service (BETA)"
+    echo "  update_repo                 Update repository"
+    echo "  android_rules               Install Android ADB rules (klipperscreen)"
+    echo "  crowsnest_fix               Install Crowsnest FPS Fix"
+    echo "  base_image_config           Base ZNP-K1 Compiled Image Config (Dont use on release images)"
+    echo "  de_elegoo_image_cleanser    Method 2 (No eMMC) - Elegoo Image Cleanser Script - Not Advised"
+    echo "  armbian_resize              Resize Active Armbian Partition (for eMMC > 8GB)"
+    echo ""
 }
 
 # Function to print the main menu
@@ -491,38 +574,78 @@ print_menu() {
     echo "======================================"
 }
 
-# Main menu loop
-while true; do
-    print_menu
-    read -p "Enter your choice: " choice
-    case $choice in
-        1)
-            install_printer_cfg
-            ;;
-        2)
-            wifi_config
-            ;;
-        3)
-            usb_auto_mount
-            ;;
-        4)
-            update_mcu_rpi_fw
-            ;;
-        5)
-            install_screen_service
-            ;;
-        6)
-            advanced_more
-            ;;
-        7)
-            update_repo
-            ;;
-        8)
-            echo "Exiting..."
-            exit 0
-            ;;
-        *)
-            echo "Invalid choice. Please try again."
-            ;;
+if [ -z "$1" ];
+then
+    run_fixes
+    update_repo
+    # Main menu loop
+    while true; do
+        print_menu
+        read -p "Enter your choice: " choice
+        case $choice in
+            1)
+                install_printer_cfg
+                ;;
+            2)
+                wifi_config
+                ;;
+            3)
+                usb_auto_mount
+                ;;
+            4)
+                update_mcu_rpi_fw
+                ;;
+            5)
+                install_screen_service
+                ;;
+            6)
+                advanced_more
+                ;;
+            7)
+                update_repo
+                ;;
+            8)
+                echo "Exiting..."
+                exit 0
+                ;;
+            *)
+                echo "Invalid choice. Please try again."
+                ;;
+        esac
+    done
+else
+    TEMP=$(getopt -o yh --long yes,help,printer_model:,motor_current:,pcb_version: \
+                -n 'OpenNept4une.sh' -- "$@")
+    if [ $? != 0 ] ; then exit 1 ; fi
+    eval set -- "$TEMP"
+
+    while true; do
+        case "$1" in
+            --printer_model ) PRINTER_MODEL="$2"; shift 2 ;;
+            --motor_current ) MOTOR_CURRENT="$2"; shift 2 ;;
+            --pcb_version ) PCB_VERSION="$2"; shift 2 ;;
+            -y|--yes ) SAY_YES=true; shift ;;
+            -h|--help ) print_help; exit 0 ;;
+            * ) break ;;
+        esac
+    done
+
+    run_fixes
+
+    COMMAND=$1;
+
+    case $COMMAND in
+        install_printer_cfg ) install_printer_cfg ;;
+        wifi_config ) wifi_config ;;
+        usb_auto_mount ) usb_auto_mount ;;
+        update_mcu_rpi_fw|update_mcu ) update_mcu_rpi_fw ;;
+        install_screen_service ) install_screen_service ;;
+        update_repo ) update_repo ;;
+        android_rules ) android_rules ;;
+        crowsnest_fix ) crowsnest_fix ;;
+        base_image_config ) base_image_config ;;
+        de_elegoo_image_cleanser ) de_elegoo_image_cleanser ;;
+        armbian_resize ) armbian_resize ;;
+        * ) echo "Invalid command. Please try again." ;;
     esac
-done
+fi
