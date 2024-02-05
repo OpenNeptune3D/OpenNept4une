1b27c3c29119b5c454d8bf1022886f3cd4d6b49b
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-02-05 13:47:36 +0000

    Issue #68 fix - de_elegoo_cleanser.sh

--- img-config/de_elegoo_cleanser.sh
+++ img-config/de_elegoo_cleanser.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# User prompt for warning and confirmation
+echo "WARNING: This script will significantly change your system's package sources and installed packages.\n\n"
+echo "This can potentially BRICK this Armbian install.\n\n"
+echo "Please ensure you understand the risks.\n"
+echo "An eMMC reader will be required if it goes wrong. So consider just flashing the OpenNept4une Image"
+
+echo "Do you wish to continue? (yes/no)"
+
+# Read user input
+read -r response
+
+# Check if the user wants to proceed
+if [[ "$response" != "yes" ]]; then
+    echo "Operation aborted by the user."
+    exit 1
+fi
+
 sudo service klipper stop 
 
 sudo systemctl start ntp
@@ -21,14 +38,6 @@ sudo service nginx stop
 sudo rm /boot/wpa_supplicant/wpa_supplicant-wlan0.conf
 sudo rm /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
 
-# sudo cp ~/OpenNept4une/img-config/spidev-fix/rockchip-spi-spidev.dtbo /boot/dtb/rockchip/overlay/
-# sudo cp ~/OpenNept4une/img-config/spidev-fix/rockchip-spi-spidev.dtbo /boot/dtb-5.16.20-rockchip64/rockchip/overlay/
-
-# sudo cp ~/OpenNept4une/img-config/spidev-fix/rockchip-fixup.scr /boot/dtb/rockchip/overlay/
-# sudo cp ~/OpenNept4une/img-config/spidev-fix/rockchip-fixup.scr /boot/dtb-5.16.20-rockchip64/rockchip/overlay/
-
-# sudo cp ~/OpenNept4une/img-config/spidev-fix/99-spidev.rules /etc/udev/rules.d/
-
 sudo cp ~/OpenNept4une/img-config/led-v1.1+fix/99-gpio.rules /etc/udev/rules.d/
 
 ~/klipper/scripts/klipper-uninstall.sh
@@ -38,8 +47,41 @@ sudo cp ~/OpenNept4une/img-config/led-v1.1+fix/99-gpio.rules /etc/udev/rules.d/
 sudo find ~/ -mindepth 1 ! -path '~/OpenNept4une*' -exec rm -rf {} +
 sudo rm -rf /root/*
 
+sudo apt remove nginx -y
+
+# TESTING Risky Bookworm Upgrade
+
+# Hold DTB, u-boot, kernel & firmware packages
+sudo apt-mark hold linux-dtb-edge-rockchip64 linux-image-edge-rockchip64 linux-libc-dev:arm64 linux-u-boot-mkspi-edge armbian-bsp-cli-mkspi armbian-firmware
+
+# Update Buster packages
+sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
+
+# Hold Armbian update sources before full-upgrade
+sed -i 's/^deb/#deb/' /etc/apt/sources.list.d/armbian.list
+
+# Update Buster sources to Bookworm
+sudo sed -i 's/buster/bookworm/g' /etc/apt/sources.list
+sudo sed -i 's/buster/bookworm/g' /etc/apt/sources.list.d/armbian.list
+echo "deb http://security.debian.org/ bookworm-security main contrib non-free" | sudo tee -a /etc/apt/sources.list
+
+# Install Bookworm packages and perform a full upgrade
+sudo apt update -y
+sudo apt full-upgrade -y
+
+# Release Armbian update sources after full-upgrade
+sed -i 's/^#deb/deb/' /etc/apt/sources.list.d/armbian.list
+
+sudo apt autoremove -y
+sync
+echo "Upgrade to Bookworm completed."
+
+
 # Create the config directory structure if not exists
 [ ! -d "$HOME/printer_data/config/" ] && mkdir -p ~/printer_data/config/
+
+# Create the database directory structure if not exists
+[ ! -d "$HOME/printer_data/database/" ] && mkdir -p ~/printer_data/database/
 
 # Clone the KAMP git repository if not exists
 if [ ! -d "$HOME/Klipper-Adaptive-Meshing-Purging" ]; then
@@ -54,77 +96,34 @@ if [ ! -d "$HOME/kiauh" ]; then
     cd ~/ && git clone https://github.com/dw-0/kiauh.git
 fi
 
-# Hardcoded list of GitHub raw links paired with filenames
-declare -A LINKS_AND_NAMES=(
-    ["https://raw.githubusercontent.com/OpenNeptune3D/OpenNept4une/main/img-config/printer-data/printer.cfg"]="printer.cfg"
-    ["https://raw.githubusercontent.com/OpenNeptune3D/OpenNept4une/main/img-config/printer-data/moonraker.conf"]="moonraker.conf"
-    ["https://raw.githubusercontent.com/OpenNeptune3D/OpenNept4une/main/img-config/printer-data/KAMP_Settings.cfg"]="KAMP_Settings.cfg"
-    ["https://raw.githubusercontent.com/OpenNeptune3D/OpenNept4une/main/img-config/printer-data/adxl.cfg"]="adxl.cfg"  
-    ["https://raw.githubusercontent.com/OpenNeptune3D/OpenNept4une/main/img-config/printer-data/crowsnest.conf"]="crowsnest.conf"
-)
+# Copy Configurations
+cp -r ~/OpenNept4une/img-config/printer-data/* ~/printer_data/config/ && \
+mv ~/printer_data/config/data.mdb ~/printer_data/database/data.mdb
 
-# Destination directory
-DEST_DIR="$HOME/printer_data/config"
-
-# Loop through each link and download the file only if it doesn't exist
-for link in "${!LINKS_AND_NAMES[@]}"; do
-    FILENAME="${LINKS_AND_NAMES[$link]}"
-    if [ ! -f "$DEST_DIR/$FILENAME" ]; then
-        wget -O "$DEST_DIR/$FILENAME" "$link"
-        echo "Downloaded $FILENAME."
-    else
-        echo "$FILENAME already exists. Skipping download."
-    fi
-done
-
-# Fluidd DB transfer
-SHARE_LINK="https://raw.githubusercontent.com/OpenNeptune3D/OpenNept4une/main/img-config/printer-data/data.mdb"
-
-DESTINATION_DIR="$HOME/printer_data/database"
-DESTINATION_FILE="${DESTINATION_DIR}/data.mdb"
-
-# Check and create the output directory if it doesn't exist
-[ ! -d "${DESTINATION_DIR}" ] && mkdir -p "${DESTINATION_DIR}"
-
-# Download the .mdb file only if it doesn't already exist
-if [ ! -f "${DESTINATION_FILE}" ]; then
-    wget -O "${DESTINATION_FILE}" "${SHARE_LINK}"
-    echo "Downloaded ${DESTINATION_FILE}."
-else
-    echo "${DESTINATION_FILE} already exists. Skipping download."
-fi
-
-sudo apt-mark hold linux-dtb-edge-rockchip64 linux-image-edge-rockchip64 linux-libc-dev:arm64 linux-u-boot-mkspi-edge armbian-bsp-cli-mkspi armbian-firmware
-
-sudo apt update 
-sudo apt remove nginx -y
 sudo apt install network-manager -y 
 sudo apt install ustreamer -y 
 sudo apt install python3-venv -y 
-sudo apt dist-upgrade -y
 sudo apt clean -y
 sudo apt autoclean -y
 sudo apt autoremove -y
 sudo rm -rf /var/log/*
 sudo rm -rf /usr/share/man/*
 
-git clone --branch legacy/v3 https://github.com/mainsail-crew/crowsnest.git
+#git clone --branch legacy/v3 https://github.com/mainsail-crew/crowsnest.git
 
 # Define the file path
-FILE="$HOME/printer_data/config/moonraker.conf"
+#FILE="$HOME/printer_data/config/moonraker.conf"
 
-# Check if the file exists
-if [[ ! -f "$FILE" ]]; then
-    echo "File not found: $FILE"
-    exit 1
-fi
+#perl -i -pe 'if (/^\[update_manager crowsnest\]/ ... /^$/) {
+#    if (/origin: https:\/\/github\.com\/mainsail-crew\/crowsnest\.git/) {
+#        print;
+#        print "primary_branch: legacy/v3\n";
+#    } else {
+#        print;
+#    }
+#}' "$FILE"
 
-# Use sed to modify the file in-place
-sed -i '/^\[update_manager crowsnest\]/,/^$/ {
-    /origin: https:\/\/github\.com\/mainsail-crew\/crowsnest\.git/ a primary_branch: legacy/v3
-}' "$FILE"
-
-echo "File updated successfully."
+#echo "File updated successfully."
 
 
 CRON_ENTRY="*/10 * * * * /bin/sync"
@@ -135,36 +134,28 @@ else
     echo "The sync command is already in the crontab."
 fi
 
+#sudo rm /etc/udev/rules.d/50-usb* 
+
 sudo nmtui
 
 clear
 
-echo "When Kiauh opens, go to the uninstall page FIRST and remove each one for final clean-up"
-echo "even if they don't look installed..."
+echo "When Kiauh opens after these instructions, go to the uninstall page FIRST and remove each one for final clean-up.\n"
+echo "even if they don't look installed... (klipper, moonraker, fluidd, fluid-config, klipper-screen, crowsnest)\n\n"
 sleep 20
-echo ""
-echo "Then Install the following, in this ORDER."
-echo "Klipper, Moonraker, Fluidd, Mainsail (on port 81) then Mobileraker"
+
+echo "Then Install the following, in this ORDER.\n"
+echo "Klipper, Moonraker, Fluidd, Mainsail (on port 81) then Mobileraker.\n\n"
+echo "After, on the main kiauh menu select Update then all with (a)\n"
+echo "then exit kiauh\n\n"
 sleep 20
-echo ""
-echo "You should then run ~/OpenNept4une/OpenNept4une.sh"
-echo ""
-echo "The main requirement here is to Install the latest OpenNept4une Printer.cfg AND Update (Virtual) MCU rpi Firmware"
-echo ""
-echo "Copy the text above for reference as it will disappear in 60s"
 
-countdown=60
-
-while [ $countdown -gt 0 ]; do
-    if (( $countdown % 5 == 0 )); then
-        echo "Waiting for $countdown seconds..."
-    fi
-
-    sleep 1
-    ((countdown--))
-done
-
-echo "Countdown finished."
+echo "You should then run ~/OpenNept4une/OpenNept4une.sh\n\n"
+echo "The main requirement here is to Install the latest OpenNept4une configurations (Option 1)\n" 
+echo "Select No (n) when asked to reboot then select - Update MCU & (Virtual) MCU rpi Firmware.\n\n"
+echo "Copy / screenshot the text above for reference as it will disappear.\n\n"
+echo "Press Enter when you are ready to continue..."
+read -r
 
 sync 
 
