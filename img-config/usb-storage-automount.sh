8e4a607d35ab84de776508f1f4a72e9f478145fd
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-02-07 10:38:30 +0000

    Update usb-storage-automount.sh

--- img-config/usb-storage-automount.sh
+++ img-config/usb-storage-automount.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
+# Remove old mount directory if it exists 
+sudo rm -f /etc/udev/rules.d/99-usb_automount.rules
 
-# Create the base mount directory if it doesn't exist
-mkdir -p ~/printer_data/gcodes/USB
-sudo rm /etc/udev/rules.d/99-usb_automount.rules
-sudo -u mks echo 'ACTION=="add", SUBSYSTEMS=="usb", SUBSYSTEM=="block", KERNEL=="sd*1", ENV{ID_FS_USAGE}=="filesystem", RUN{program}+="/usr/bin/systemd-mount --no-block --automount=yes --collect --options sync,flush,nofail $devnode /home/mks/printer_data/gcodes/USB"' | sudo tee -a /etc/udev/rules.d/99-usb_automount.rules
+# Define the path for the udev rule file
+udev_rule_path="/etc/udev/rules.d/99-usb_automount.rules"
+
+# Define the mount point
+mount_point="/home/mks/printer_data/gcodes/USB"
+
+# Obtain UID and GID for the mks user
+uid=$(id -u mks)
+gid=$(id -g mks)
+
+# Udev rule to add
+udev_rule_add="ACTION==\"add\", SUBSYSTEMS==\"usb\", SUBSYSTEM==\"block\", KERNEL==\"sd*1\", ENV{ID_FS_USAGE}==\"filesystem\", RUN{program}+=\"/usr/bin/systemd-mount --no-block --automount=yes --collect --options uid=$uid,gid=$gid,sync,nofail \$devnode $mount_point\""
+
+# Udev rule to remove
+udev_rule_remove="ACTION==\"remove\", SUBSYSTEMS==\"usb\", SUBSYSTEM==\"block\", KERNEL==\"sd*1\", RUN+=\"/bin/sh -c '/bin/umount $mount_point'\""
+
+# Write the rules to the udev rule file
+{
+    echo "$udev_rule_add"
+    echo "$udev_rule_remove"
+} | sudo tee "$udev_rule_path"
+
+# Create the mount point directory if it doesn't exist
+if [ ! -d "$mount_point" ]; then
+    sudo mkdir -p "$mount_point"
+fi
+
+# Change the ownership of the mount point to user 'mks'
+sudo chown mks:mks "$mount_point"
 
 # Reload udev rules
 sudo udevadm control --reload-rules && sudo udevadm trigger
 
-echo "Udev rule for USB automount with read-only option is configured."
+echo "Udev rule for USB automount with sync option is configured."
