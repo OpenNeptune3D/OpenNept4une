9f39d8293f8468484fb7a28255b67bb3cce3630f
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-02-09 13:15:24 +0000

    Update OpenNept4une.sh

--- OpenNept4une.sh
+++ OpenNept4une.sh
@@ -646,7 +646,7 @@ if [ -z "$1" ]; then
             5) install_screen_service ;;
             6) advanced_more ;;
             q) echo -e "${G}Goodbye...${NC}"; exit 0 ;;
-            *) echo -e "${R}Invalid choice. Please try again.${NC}" ;;
+            *) clear; echo -e "${G}Goodbye...${NC}"; exit 0 ;;
         esac
     done
 else
