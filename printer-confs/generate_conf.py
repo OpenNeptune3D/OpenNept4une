16fd515ca41d4207c498fa549d2c464bb43ac458
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-02-20 12:49:46 +0000

    FIX run script from any dir

--- printer-confs/generate_conf.py
+++ printer-confs/generate_conf.py
@@ -5,28 +5,29 @@ import os
 variable_regex = re.compile(r'{{\s*(\w+)\s*}}')
 
 def get_printer_conf(printer_model, current):
-    folder = printer_model + '/'
+    folder = os.path.join(script_dir, printer_model) + '/'  # Use script_dir to construct the folder path
     if current is not None:
-        printer_conf = open(folder + printer_model + '.cfg', 'r').readlines()
-        printer_conf += open(folder + current + '.cfg', 'r').readlines()
+        printer_conf = open(os.path.join(folder, printer_model + '.cfg'), 'r').readlines()
+        printer_conf += open(os.path.join(folder, current + '.cfg'), 'r').readlines()
         return printer_conf
     else:
-        return open(folder + printer_model + '.cfg', 'r').readlines()
+        return open(os.path.join(folder, printer_model + '.cfg'), 'r').readlines()
 
 def generate_conf(printer_model, current):
     print("Generating config for " + printer_model + ("" if current is None else " with current " + current))
-    base_conf = open('base.cfg', 'r').read()
+    base_conf_path = os.path.join(script_dir, 'base.cfg')  # Construct the path to base.cfg
+    base_conf = open(base_conf_path, 'r').read()
     printer_conf = get_printer_conf(printer_model, current)
     for line in printer_conf:
         split = line.split('=')
         if len(split) > 1:
             base_conf = base_conf.replace('{{ ' + split[0] + ' }}', split[1].strip())
-    
-    section_files = os.listdir(printer_model)
-    for section_file in section_files :
+
+    section_files = os.listdir(os.path.join(script_dir, printer_model))  # Use script_dir to list section files
+    for section_file in section_files:
         if section_file.startswith('section_') and section_file.endswith('.cfg'):
             section_name = section_file.replace('section_', '').replace('.cfg', '')
-            section_conf = open(printer_model + '/' + section_file, 'r').read()
+            section_conf = open(os.path.join(script_dir, printer_model, section_file), 'r').read()
             base_conf = base_conf.replace('{{ ' + section_name + ' }}', section_conf.strip())
 
     result = re.findall(variable_regex, base_conf)
@@ -35,12 +36,12 @@ def generate_conf(printer_model, current):
         for group in result:
             print("    " + group)
         return
-    with open('output.cfg', 'w') as output:
+    with open(os.path.join(script_dir, 'output.cfg'), 'w') as output:  # Save output.cfg in the script's directory
         output.write(base_conf)
-    print("Config generated at output.cfg")
-
+    print("Config generated at " + os.path.join(script_dir, 'output.cfg'))
 
 if __name__ == '__main__':
+    script_dir = os.path.dirname(os.path.abspath(__file__))  # Get the directory where the script is located
     printer_model = argv[1]
     current = argv[2] if len(argv) > 2 else None
     generate_conf(printer_model, current)
