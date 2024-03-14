509ec7cbcd7ffb83fef34a4a10abd022bed7e115
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-03-14 15:17:08 +0000

    SuperCapacitor Safe Poweroff, Wiggle Resume Purge & New Orca Profiles + Fixes (#147)
    * power_monitor.service (safe shutdown on powerloss) OpenNept4une.sh
    
    * Added Elegoo 'wiggle' purge in PRINT_RESUME base.cfg
    
    * PRINT_PAUSE updated to wipe out to reduce stringing 
    
    * PRINT_RESUME updated to move back to the part then lower z (reduce the chance of filament blob sticking to an exterior perimeter)
    
    * Added suggested PETG values within the comments after each firmware retraction variable.
    
    * Changed probe samples_tolerance: from 0.05 to 0.025
    
    New Orca Profiles: 
    Bundled PETG Profiles 
    Warp reduction - Reverse on odd (internal wall), 
    Neater Seams - Scarf joint seam
    Prevent over extrusion in small areas - Small area flow compensation.
    
    Fixed: 
    
    * (Orca Error) Travel accel (5000) higher than the machine limit (3000)
    * (Klipper deprecated) max_accel_to_decel removed from base.cfg
    
    https://www.klipper3d.org/Config_Changes.html?h=max_accel_to_decel
    
    Replaced by default - minimum_cruise_ratio: 0.5
    
    https://www.klipper3d.org/Config_Reference.html#printer

---
+++ img-config/power_monitor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Enable / Charge SuperCapacitor
+gpioset gpiochip1 21=1
+
+# Function to handle power-cut
+handle_power_cut() {
+    # Then, initiate a safe shutdown
+    sudo poweroff
+}
+
+# Monitor GPIO line 10 for changes in the background
+gpiomon --num-events=1 --rising-edge gpiochip1 10 &
+# Monitor GPIO line 19 for changes in the background
+gpiomon --num-events=1 --falling-edge gpiochip1 19 &
+# Wait for any gpiomon process to exit
+wait
+# Call the handle function
+handle_power_cut
